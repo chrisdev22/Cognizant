096847a967302d2395128b88a018871f0eaa711f
Christian <christian@Christians-iMac-2.local>
2020-04-27 21:11:49 -0400

    React Game Project

---
+++ build/precache-manifest.7337bf517e38a2fce6d18aa6b1e24b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e1530adb8d297cc51daf924817b763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be22eb92285ed1c9fd2c",
+    "url": "/static/css/main.5824bd15.chunk.css"
+  },
+  {
+    "revision": "e9f8e18ba4c9486bf4fc",
+    "url": "/static/js/2.45f16544.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.45f16544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be22eb92285ed1c9fd2c",
+    "url": "/static/js/main.648202da.chunk.js"
+  },
+  {
+    "revision": "1d108d22457b3cedaade",
+    "url": "/static/js/runtime-main.3d501923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
